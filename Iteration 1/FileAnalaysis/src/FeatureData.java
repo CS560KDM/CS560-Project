e66842b1ece3af9b940efb49fe3658f5559bdea3
pv6xc <pv6xc@mail.umkc.edu>
2014-03-11 22:32:58 -0400

    adding iteration 1

---
+++ Iteration 1/FileAnalaysis/src/FeatureData.java
@@ -0,0 +1,35 @@
+
+public class FeatureData {
+
+	//Double norm;
+	Double sumOfNorm;
+	Integer numberOfPeaks;
+	Double distanceTravelled;
+	long patternTime;
+	public Double getSumOfNorm() {
+		return sumOfNorm;
+	}
+	public void setSumOfNorm(Double sumOfNorm) {
+		this.sumOfNorm = sumOfNorm;
+	}
+	public Integer getNumberOfPeaks() {
+		return numberOfPeaks;
+	}
+	public void setNumberOfPeaks(Integer numberOfPeaks) {
+		this.numberOfPeaks = numberOfPeaks;
+	}
+	public Double getDistanceTravelled() {
+		return distanceTravelled;
+	}
+	public void setDistanceTravelled(Double distanceTravelled) {
+		this.distanceTravelled = distanceTravelled;
+	}
+	public long getPatternTime() {
+		return patternTime;
+	}
+	public void setPatternTime(long patternTime) {
+		this.patternTime = patternTime;
+	}
+	
+	
+}
